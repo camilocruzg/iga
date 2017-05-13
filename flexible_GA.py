5b9b51c7843d7a4c9c3f824f51193434cd7c3eb2
JunyuanLiu <liujunyuan09@gmail.com>
2017-05-13 15:49:45 +1000

    Add: new fitness function: spaces()

--- flexible_GA.py
+++ flexible_GA.py
@@ -3,6 +3,37 @@ import sys
 import pickle
 from Init_Ind import Individual
 
+### returns Depth first search for graphs (returns sub-graphs of connected nodes)
+def dfs(g, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(g[vertex] - visited)
+    v_dict = {}
+    for i in visited:
+        v_dict[i]=g[i]
+    return v_dict
+
+### returns the clusters from a graph
+def clusters(g):
+    clust = []
+    for node in g:
+            c = dfs(g,node)
+            if c not in clust:
+                clust.append(c)
+    return clust
+
+def spaces(ind):
+    g = ind.graph()
+    c = clusters(g)
+    possible_clusters = ind.s[0]*ind.s[1]*ind.s[2]
+    ind.values['space'] = "%.3f" %((float(len(c)) - 1.0)/(float(possible_clusters)-1.0))
+    return ind.values['space']
+
+
 def walkability(ind):
     g = ind.graph()
 
@@ -65,41 +96,6 @@ def structure(ind):
             cols[i] = 0
     ind.values['structure'] = "%.3f" % (sum(cols) / float(len(cols)))
     return ind.values['structure']
-# def structure(ind):
-#     cols = []
-#     for i in range(int((ind.s[0] + 1) * (ind.s[1] + 1))):
-#         cols.append(0)
-#     for i, col in enumerate(cols):
-#         x = int(i % (ind.s[0] + 1))
-#         y = int(i // (ind.s[0] + 1))
-#         neigh = []  # right, front, left, back
-#         if x < ind.s[0]:  # right
-#             neigh.append(int(x + ind.xy + (y * (ind.s[0] * ind.s[2]))))
-#         if y < ind.s[1]:
-#             neigh.append(int(y + ind.xy + ind.xz + (x * (ind.s[1] * ind.s[2]))))
-#         if x > 0:
-#             neigh.append(int(x - 1 + ind.xy + (y * (ind.s[0] * ind.s[2]))))
-#         if y > 0:
-#             neigh.append(int(y - 1 + ind.xy + ind.xz + (x * (ind.s[1] * ind.s[2]))))
-#         z = 0
-#         while z < int(ind.s[2]):
-#             n = []
-#             for j in neigh:
-#                 if j < (ind.xy + ind.xz):
-#                     n.append(ind[j + (z * int(ind.s[0]))])
-#                 else:
-#                     n.append(ind[j + (z * int(ind.s[1]))])
-#             if sum(n) > 0:
-#                 cols[i] += 1
-#             z += 1
-#     for i in range(len(cols)):
-#         if cols[i] == ind.s[2]:
-#             cols[i] = 1
-#         else:
-#             cols[i] = 0
-#
-#     ind.values['structure'] = "%.3f" % (sum(cols) / float(len(cols)))
-#     return ind.values['structure']
 
 def hamming_dist(pop):
     for i, ind in enumerate(pop):
@@ -178,7 +174,6 @@ def tournament(pop, p_len, problem):
             _pop.remove(other)
     return new_pop
 
-
 def mate(pop):
     new_pop = []
     ### This step makes the length of the population even
@@ -236,6 +231,7 @@ def evolve(pop_size, ind_size, gens, problem, pop=[]):
 
         new_pop = pop
         for i in new_pop:
+            spaces(i)
             walkability(i)
             structure(i)
         hamming_dist(new_pop)
@@ -265,6 +261,7 @@ def evolve(pop_size, ind_size, gens, problem, pop=[]):
             for i in new_pop:
                 walkability(i)
                 structure(i)
+                spaces(i)
             hamming_dist(new_pop)
             fronts2 = sort_pop_st(new_pop, problem)
             new_pop = [ind for front in fronts2 for ind in front]
@@ -273,7 +270,8 @@ def evolve(pop_size, ind_size, gens, problem, pop=[]):
             print "  Number %dth loop is done"%(gens)
 
             gens -= 1
-
+        for each in  new_pop[:10]:
+            print each.values
         # output = pickle.dumps(new_pop[:10])
         output = pickle.dumps(new_pop)
         # print type(new_pop)
