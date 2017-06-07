48a28909b1ce255ceca32520a5dc79f3082cda1c
JunyuanLiu <liujunyuan09@gmail.com>
2017-06-07 10:36:51 +1000

    Reformat code

--- TCPserver_threaded.py
+++ TCPserver_threaded.py
@@ -5,7 +5,10 @@ import flexible_GA
 import sys
 import struct
 import pickle
+import time
 
+
+# the server class
 class ThreadedServer(object):
     def __init__(self, host, port):
         self.host = host
@@ -14,20 +17,22 @@ class ThreadedServer(object):
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
 
+    # main function to start multi-threaded server
     def listen(self):
         self.sock.listen(5)
-        print "the server is listening..."
+        print
+        "the server is listening..."
         while True:
             connect, address = self.sock.accept()
-            threading.Thread(target = self.listenToClient, args = (connect,address)).start()
+            threading.Thread(target=self.listenToClient, args=(connect, address)).start()
 
+    # Prefix each message with a 4-byte length (network byte order)
     def send_msg(self, sock, msg):
-        # Prefix each message with a 4-byte length (network byte order)
         msg = struct.pack('>I', len(msg)) + msg
         sock.sendall(msg)
 
-    def recv_msg(self,sock):
-        # Read message length and unpack it into an integer
+    # Read message length and unpack it into an integer
+    def recv_msg(self, sock):
         raw_msglen = self.recv_all(sock, 4)
         if not raw_msglen:
             return None
@@ -35,8 +40,8 @@ class ThreadedServer(object):
         # Read the message data
         return self.recv_all(sock, msglen)
 
+    # Helper function to recv n bytes or return None if EOF is hit
     def recv_all(self, sock, prefix_len):
-        # Helper function to recv n bytes or return None if EOF is hit
         data = ''
         while len(data) < prefix_len:
             packet = sock.recv(prefix_len - len(data))
@@ -45,44 +50,44 @@ class ThreadedServer(object):
             data += packet
         return data
 
-    def listenToClient(self, sock,address):
+    # call the genetic algorithm when request received
+    def listenToClient(self, sock, address):
         try:
             # data = (connect.recv(8192)).strip()
             recv_data = self.recv_msg(sock).strip()
 
             if recv_data:
-                print threading.current_thread().getName() + " is receiving request with " + recv_data
-                # print data
+                print
+                threading.current_thread().getName() + " is receiving request with " + recv_data
+                start_t = time.time()
                 recv_data = eval(recv_data)
-                # print type(recv_data)
-
                 pop_list = []
                 if recv_data["type"] == "Init":
                     # print threading.current_thread().getName() + " start initializing "
                     pop_list = flexible_GA.evolve(recv_data["popsize"], recv_data["indsize"], recv_data["gens"],
-                                                  recv_data["problem"],recv_data['selection'],pop = [])
+                                                  recv_data["problem"], recv_data['selection'], pop=[])
                     # print threading.current_thread().getName() + " the size of result is " + str(sys.getsizeof(poplist))
                     # print threading.current_thread().getName() + " all good"
 
                 elif recv_data["type"] == "Seed":
                     the_seed = pickle.loads(recv_data["pop"])
-                    # print the_seed
                     pop_list = flexible_GA.evolve(recv_data["popsize"], recv_data["indsize"], recv_data["gens"],
-                                                 recv_data["problem"],recv_data['selection'], the_seed)
-
-
+                                                  recv_data["problem"], recv_data['selection'], the_seed)
                 self.send_msg(sock, pop_list)
-
-                print threading.current_thread().getName() + " send the data with size of " + str(
+                end_t = time.time()
+                print
+                threading.current_thread().getName() + " send the data of size: " + str(
                     sys.getsizeof(pop_list))
-                # print threading.current_thread().isAlive()
+                print
+                threading.current_thread().getName() + " takes %0.3f" % (end_t - start_t) + " seconds"
 
 
         except Exception as ex:
-            print threading.current_thread().getName() + " is quiting with error: ", ex
+            print
+            threading.current_thread().getName() + " is quiting with error: ", ex
             sock.close()
             return False
 
 
 if __name__ == "__main__":
-    ThreadedServer('',1337).listen()
+    ThreadedServer('', 1337).listen()
--- flexible_GA.py
+++ flexible_GA.py
@@ -1,9 +1,14 @@
+# for generate pseudo random number. Used for generate population,
 from random import randint, random, sample
-import sys
+# to serialize the data to make it transmissible
 import pickle
+# the user-defined class to instantiate "Individual" objects
 from Init_Ind import Individual
+# to log and print status
+import sys
 
-### returns Depth first Search for graphs (returns sub-graphs of connected nodes)
+
+# returns Depth first Search for graphs (returns sub-graphs of connected nodes)
 def dfs(g, start):
     visited = set()
     stack = [start]
@@ -14,25 +19,30 @@ def dfs(g, start):
             stack.extend(g[vertex] - visited)
     v_dict = {}
     for i in visited:
-        v_dict[i]=g[i]
+        v_dict[i] = g[i]
     return v_dict
 
-### returns the clusters from a graph
+
+# returns the clusters from a graph, used for space()
 def clusters(g):
     clust = []
     for node in g:
-            c = dfs(g,node)
-            if c not in clust:
-                clust.append(c)
+        c = dfs(g, node)
+        if c not in clust:
+            clust.append(c)
     return clust
 
+
+# fitness function: spatial continuity
 def spaces(ind):
     g = ind.graph()
     c = clusters(g)
-    possible_clusters = ind.s[0]*ind.s[1]*ind.s[2]
-    ind.values['space'] = "%.3f" %((float(len(c)) - 1.0)/(float(possible_clusters)-1.0))
+    possible_clusters = ind.s[0] * ind.s[1] * ind.s[2]
+    ind.values['space'] = "%.3f" % ((float(len(c)) - 1.0) / (float(possible_clusters) - 1.0))
     return ind.values['space']
 
+
+# fitness function: walkability
 def walkability(ind):
     g = ind.graph()
 
@@ -57,10 +67,12 @@ def walkability(ind):
             if i - ind.s[0] in g[i] and ind[i - ind.s[0]] == 1:
                 deg += 1
         w.append(float(deg) / pot)
-    ar = sum(w) / len(g.keys())  #accessibility ratio (how much of the potential accessibility is reached)
+    ar = sum(w) / len(g.keys())  # accessibility ratio (how much of the potential accessibility is reached)
     ind.values['walk'] = "%.3f" % ar
     return ind.values['walk']
 
+
+# fitness function: structure feasibility
 def structure(ind):
     cols = []
     for i in range(int((ind.s[0] + 1) * (ind.s[1] + 1))):
@@ -96,6 +108,8 @@ def structure(ind):
     ind.values['structure'] = "%.3f" % (sum(cols) / float(len(cols)))
     return ind.values['structure']
 
+
+# calculate the hamming distance. Used when domination ties.
 def hamming_dist(pop):
     for i, ind in enumerate(pop):
         hd = []
@@ -109,6 +123,8 @@ def hamming_dist(pop):
         ind.hd = sum(hd) / (len(pop) - 1)
     return pop
 
+
+# sort population
 def sort_pop_st(pop, problem):
     # new_pop is a list of pointers to the individuals in pop
     pop_ind = [i for i in range(len(pop))]  ###List of pointers to individuals in pop
@@ -150,6 +166,9 @@ def sort_pop_st(pop, problem):
     ff = [[pop[i] for i in f] for f in fronts]
     return ff
 
+
+# a dominance-based system: randomly chose two individual from the population and compete on against the other.
+# pick the winner who dominates the other one.
 def tournament(pop, p_len, problem):
     _pop = pop
     new_pop = []
@@ -173,6 +192,8 @@ def tournament(pop, p_len, problem):
             _pop.remove(other)
     return new_pop
 
+
+# 4-point cross-over mate
 def mate(pop):
     new_pop = []
     ### This step makes the length of the population even
@@ -184,14 +205,18 @@ def mate(pop):
     xy = pop[0].xy
     xz = xy + pop[0].xz
     while len(pop_i) > 0:
-        parent1, parent2 = sample(pop_i,2)
+        parent1, parent2 = sample(pop_i, 2)
         p0 = randint(0, xy)
         p1 = randint(xy, xz)
         p2 = randint(xy, xz)
         p3 = randint(xz, pop[parent1].getLen())
         p = sorted([p0, p1, p2, p3])
-        ni1 = pop[parent1][:p[0]] + pop[parent2][p[0]:p[1]] + pop[parent1][p[1]:p[2]] + pop[parent2][p[2]:p[3]] + pop[parent1][p[3]:]
-        ni2 = pop[parent2][:p[0]] + pop[parent1][p[0]:p[1]] + pop[parent2][p[1]:p[2]] + pop[parent1][p[2]:p[3]] + pop[parent2][p[3]:]
+        ni1 = pop[parent1][:p[0]] + pop[parent2][p[0]:p[1]] + pop[parent1][p[1]:p[2]] + pop[parent2][p[2]:p[3]] + pop[
+                                                                                                                      parent1][
+                                                                                                                  p[3]:]
+        ni2 = pop[parent2][:p[0]] + pop[parent1][p[0]:p[1]] + pop[parent2][p[1]:p[2]] + pop[parent1][p[2]:p[3]] + pop[
+                                                                                                                      parent2][
+                                                                                                                  p[3]:]
         new_ind1 = Individual(pop[0].s, pop[0].p, ni1)
         new_ind2 = Individual(pop[0].s, pop[0].p, ni2)
         new_pop.append(new_ind1)
@@ -199,14 +224,18 @@ def mate(pop):
         pop_i.remove(parent1), pop_i.remove(parent2)
     return new_pop
 
+
+# mutate the bit in individual with a rate of 5%
 def mutate(pop, prob):
     for ind in pop:
-        for i,bit in enumerate(ind):
+        for i, bit in enumerate(ind):
             r = random()
             if r < prob:
                 ind.c[i] = abs(bit - 1)
     return pop
 
+
+# parse json file
 def get_json(pop):
     output = []
     for each in pop:
@@ -216,7 +245,9 @@ def get_json(pop):
         output.append(ind_dict)
     return output
 
-def fit_function(pop,selection):
+
+# used for fitness function selection.
+def fit_function(pop, selection):
     for each in pop:
         if selection[0] == 1:
             walkability(each)
@@ -225,6 +256,8 @@ def fit_function(pop,selection):
         if selection[2] == 1:
             spaces(each)
 
+
+# the main function for genetic algorithm
 def evolve(pop_size, ind_size, gens, problem, selection, pop=[]):
     try:
         gens = int(gens)
@@ -234,10 +267,10 @@ def evolve(pop_size, ind_size, gens, problem, selection, pop=[]):
         print "the number of seeds is ", len(pop)
         while len(pop) < pop_size:
             pop.append(Individual(ind_size))
-        print "individual length",pop[1].l
+        # print "individual length",pop[1].l
 
         new_pop = pop
-        fit_function(new_pop,selection)
+        fit_function(new_pop, selection)
 
         hamming_dist(new_pop)
 
@@ -264,7 +297,7 @@ def evolve(pop_size, ind_size, gens, problem, selection, pop=[]):
                 ind.clear_values()
             ### re_evaluate new_pop
 
-            fit_function(new_pop,selection)
+            fit_function(new_pop, selection)
 
             hamming_dist(new_pop)
             fronts2 = sort_pop_st(new_pop, problem)
@@ -279,11 +312,11 @@ def evolve(pop_size, ind_size, gens, problem, selection, pop=[]):
         return output
 
     except Exception as ex:
-        print "evolve -> " , ex
+        print "evolve -> ", ex
         print "evolve -> " + str(sys.exc_traceback.tb_lineno)
 
 
-
+# test case
 if __name__ == '__main__':
     s = str((2, 3, 4))
-    evolve(50, s, 5,'max', [1,1,0])
+    evolve(50, s, 5, 'max', [1, 1, 0])
